ec563174475bed2996e49161da1ceee65922861e
Juan P Lopez <dolcalmi@gmail.com>
2021-01-26 16:56:49 -0500

    ghpages

---
+++ app/controllers/application.js
@@ -0,0 +1,6 @@
+import Controller from '@ember/controller';
+import { tracked } from '@glimmer/tracking';
+
+export default class ApplicationController extends Controller {
+  @tracked colors = ['#fcb851', '#e64053', '#55bcb6', '#77edb2'];
+}
