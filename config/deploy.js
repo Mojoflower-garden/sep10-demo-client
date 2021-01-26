42165720186a8b427b712ce12e1ccfe16ec7cf21
Juan P Lopez <dolcalmi@gmail.com>
2021-01-26 17:05:25 -0500

    update deploy settings

--- config/deploy.js
+++ config/deploy.js
@@ -19,8 +19,13 @@ module.exports = function (deployTarget) {
 
   if (deployTarget === 'production') {
     ENV.build.environment = 'production';
-    // configure other plugins for production deploy target here
+    ENV.rootURL = '/sep10-demo-client';
+    ENV.locationType = 'hash';
   }
+
+  ENV.git = {
+    repo: 'git@github.com:dolcalmi/sep10-demo-client.git',
+  };
 
   // Note: if you need to build some configuration asynchronously, you can return
   // a promise that resolves with the ENV object instead of returning the
