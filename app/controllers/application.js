d0df5835af736e6bf98e53156e08a50fdc3c14c6
Juan P Lopez <dolcalmi@gmail.com>
2021-01-26 17:06:12 -0500

    Deployed 4216572

--- app/controllers/application.js
+++
@@ -1,6 +0,0 @@
-import Controller from '@ember/controller';
-import { tracked } from '@glimmer/tracking';
-
-export default class ApplicationController extends Controller {
-  @tracked colors = ['#fcb851', '#e64053', '#55bcb6', '#77edb2'];
-}
